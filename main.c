96cbd908f291bbdf09fdbee957da2214bd601040
Chris Wilhelm <cwilhelm20@gmail.com>
2025-01-27 19:13:33 -0600

    Changing functionality, currently binding to known port

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /*
-** showip.c -- show IP addresses for a host given on the command line
+** This is a personal project to write an HTTP server in C.
 */
 
-#include <stdio.h>
+
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -12,49 +12,19 @@
 
 int main(int argc, char *argv[])
 {
+    int s;
+    int sockfd;
     struct addrinfo hints, *res, *p;
     int status;
     char ipstr[INET6_ADDRSTRLEN];
 
-    if (argc != 2) {
-        fprintf(stderr,"usage: showip hostname\n");
-        return 1;
-    }
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
-    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
-        //fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-        fprintf(stderr, "Address for %s not found\n", argv[1]);
-        return 2;
-    }
-
-    printf("IP addresses for %s:\n\n", argv[1]);
-
-    for(p = res;p != NULL; p = p->ai_next) {
-        void *addr;
-        char *ipver;
-
-        // get the pointer to the address itself,
-        // different fields in IPv4 and IPv6:
-        if (p->ai_family == AF_INET) { // IPv4
-            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-            addr = &(ipv4->sin_addr);
-            ipver = "IPv4";
-        } else { // IPv6
-            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
-            addr = &(ipv6->sin6_addr);
-            ipver = "IPv6";
-        }
-
-        // convert the IP to a string and print it:
-        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
-        printf("  %s: %s\n", ipver, ipstr);
-    }
-
-    freeaddrinfo(res); // free the linked list
+    getaddrinfo(NULL, "http", &hints, &res);
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    bind(sockfd, res->ai_addr, res->ai_addrlen);
 
     return 0;
 }
